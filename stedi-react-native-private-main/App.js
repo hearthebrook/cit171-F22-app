6b47865df5c7b7d770d76872f1b8fb4f2de4a446
Brooklynn Hathaway <hearthebrook@gmail.com>
2022-11-01 14:55:31 -0600

    worked on App file

--- stedi-react-native-private-main/App.js
+++ stedi-react-native-private-main/App.js
@@ -5,6 +5,123 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
+import { set } from 'react-native-reanimated';
+const AppStack = createNativeStackNavigator();
+const loggedInStates={
+  NOT_LOGGED_IN: 'NOT_LOGGED_IN',
+  LOGGED_IN: 'LOGGED_IN',
+  CODE_SENT: 'CODE_SENT'
+}
+const App = () =>{
+  const [isFirstLaunch, setFirstLaunch] = React.useState(true);
+  const [loggedInState, setLoggedInState] = React.useState(loggedInStates.NOT_LOGGED_IN);
+  const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [oneTimePassword, setOneTimePassword] = React.useState(null);
+   if (isFirstLaunch == true){
+return(
+  <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
+);
+  }else if(loggedInState == loggedInStates.LOGGED_IN){
+    return <Navigation/>
+  }else if (loggedInState == loggedInStates.NOT_LOGGED_IN){
+  return(
+    <View>
+      <TextInput
+      placeholder='Phone Number'
+      style={styles.input}
+      placeholderTextColor='#4251F5'
+      value={phoneNumber}
+      onChangeText={setPhoneNumber}
+      ></TextInput>
+      <Button
+        title='Send'
+        style={styles.Button}
+        onPress={async()=>{
+          console.log('Button was pressed!')
+          await fetch('https://dev.stedi.me/twofactorlogin/' + phoneNumber,
+          {
+            method:'POST',
+            headers:{
+              'content-type':'application/text'
+            }
+          })
+        setLoggedInState(loggedInStates.CODE_SENT);
+      }}
+      />
+    </View>
+  )}
+  else if(loggedInState == loggedInStates.CODE_SENT){
+    return(
+      <View>
+        <TextInput
+          placeholder='One Time Password'
+          style={styles.input}
+          placeholderTextColor='#4251F5'
+          value={oneTimePassword}
+          onChangeText={setOneTimePassword}
+          keyboardType="numeric"
+          >
+        </TextInput>
+        <Button
+        title='Login'
+        style={styles.Button}
+        onPress={async()=>{
+          console.log('Login Button was pressed!')
+          const loginResponse=await fetch('https://dev.stedi.me/twofactorlogin',
+          {
+            method:'POST',
+            headers:{
+              'content-type':'application/text'
+            },
+            body: JSON.stringify({
+              phoneNumber,
+              oneTimePassword
+            })
+          }
+          );
+        if(loginResponse.status==200){
+          setLoggedInState(loggedInStates.LOGGED_IN);
+        }else{
+          setLoggedInState(NOT_LOGGED_IN);
+        }
+      }}
+      />
+      </View>
+    )
+  }
+}
+export default App;
+const styles = StyleSheet.create({
+  container:{
+      flex:1,
+      alignItems:'center',
+      justifyContent: 'center'
+  },
+  input: {
+    height: 40,
+    marginTop: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  margin:{
+    marginTop:100
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }
+})
+
+/*import React, { useEffect, useState, } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button } from 'react-native';
+import  Navigation from './components/Navigation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardingScreen from './screens/OnboardingScreen';
+import Home from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
 
 
 
@@ -75,3 +192,4 @@ const styles = StyleSheet.create({
     padding: 10
   }    
 })
+*/
